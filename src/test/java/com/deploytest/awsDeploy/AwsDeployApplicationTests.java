b9c37ffe56d4d8aa2362eae792a1f2aa9525a21f
penrose15 <seongju99@naver.com>
2022-08-19 09:16:11 +0900

    init

---
+++ src/test/java/com/deploytest/awsDeploy/AwsDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploytest.awsDeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
